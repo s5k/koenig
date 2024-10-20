4f8eef9ece2803aa533af649d21c4ee671afd1ad
phongtran <phongtran81.work@gmail.com>
2024-10-20 10:39:59 +0700

    add mention node

--- packages/koenig-lexical/src/nodes/MinimalNodes.js
+++ packages/koenig-lexical/src/nodes/MinimalNodes.js
@@ -1,9 +1,11 @@
 import {LinkNode} from '@lexical/link';
+import {MentionNode} from './MentionNode.js';
 import {TKNode} from '@tryghost/kg-default-nodes';
 
 const MINIMAL_NODES = [
     LinkNode,
-    TKNode
+    TKNode,
+    MentionNode
 ];
 
 export default MINIMAL_NODES;
